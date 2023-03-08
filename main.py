5df7caf2e7580539c2a68d71fc2e2eb701a9038e
huskyHsu <schi.hsu@gmail.com>
2023-03-08 17:10:04 +0800

    feat: 🎸 add query rate limit

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from translator import ChatGPT
 if __name__ == "__main__":
     translator = ChatGPT("config.ini")
     text = translator.translate("Hello, how are you?")
-
     print(text)
+
+    for i in range(5):
+        print(translator.translate("Hello"))
--- translator/chatGPT.py
+++ translator/chatGPT.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser
 from itertools import cycle
 from typing import Iterable
+import time
 
 import requests
 from pydantic import BaseModel
@@ -11,6 +12,9 @@ class ChatGPT(BaseModel):
     language: str
     api_keys: Iterable[str]
     base_payload: dict = {}
+
+    rate_limit: float
+    last_request: dict = {}
 
     @classmethod
     def _process_payload_config(self, config):
@@ -29,14 +33,27 @@ class ChatGPT(BaseModel):
         config.read(config_path)
         language = config.get("translation", "language", fallback="Traditional Chinese")
         api_keys = cycle(config.get("openai", "api_key").split(","))
+        rate_limit = 60.0 / int(config.getint("openai", "rate_limit", fallback="20"))
         base_payload = self._process_payload_config(config)
 
         super().__init__(
-            api_keys=api_keys, base_payload=base_payload, language=language
+            api_keys=api_keys,
+            base_payload=base_payload,
+            language=language,
+            rate_limit=rate_limit,
         )
 
     def _get_header(self):
-        return {"Authorization": f"Bearer {next(self.api_keys)}"}
+        token = next(self.api_keys)
+        now = time.time()
+        if (
+            token in self.last_request
+            and now - self.last_request[token] < self.rate_limit
+        ):
+            sleep_time = self.rate_limit - (now - self.last_request[token])
+            time.sleep(sleep_time * 1.2)
+
+        return {"Authorization": f"Bearer {token}"}
 
     def translate(self, message):
         data = {
@@ -49,8 +66,11 @@ class ChatGPT(BaseModel):
                 {"role": "user", "content": message},
             ],
         }
-        response = requests.post(self.url, headers=self._get_header(), json=data)
+        headers = self._get_header()
+        response = requests.post(self.url, headers=headers, json=data)
         response.raise_for_status()
         result = response.json()
+
+        self.last_request[headers["Authorization"].split()[1]] = time.time()
 
         return result["choices"][0]["message"]["content"].strip()
